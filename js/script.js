c5228b8f41bba9937a8b2a75bbe7186cc12c4867
Derakios <kiki56860@gmail.com>
2022-02-18 16:19:55 +0100

    responsive album
    - responsive page album
    - changement du lien actif dans la navbar

--- js/script.js
+++ js/script.js
@@ -49,3 +49,63 @@ function validateContact() {
     }
 
 }
+
+var body = document.getElementById('bodyid');
+var btndis = document.getElementById('btndis');
+
+var jup = document.getElementById('imgJup2');
+var mer = document.getElementById('imgMerc2');
+var nep = document.getElementById('imgNep2');
+var sat = document.getElementById('imgSat2');
+var ven = document.getElementById('imgVen2');
+
+// Fonction image 2 de jupiter
+function appearimageJup() {
+    jup.style.display = "block";
+    btndis.style.display = "block";
+    body.style.opacity = "0.5";
+    body.style.pointerEvents = "none";
+}
+
+// Fonction image 2 de mercure
+function appearimageMer() {
+    mer.style.display = "block";
+    btndis.style.display = "block";
+    body.style.opacity = "0.5";
+    body.style.pointerEvents = "none";
+}
+
+// Fonction image 2 de neptune
+function appearimageNep() {
+    nep.style.display = "block";
+    btndis.style.display = "block";
+    body.style.opacity = "0.5";
+    body.style.pointerEvents = "none";
+}
+
+// Fonction image 2 de saturne
+function appearimageSat() {
+    sat.style.display = "block";
+    btndis.style.display = "block";
+    body.style.opacity = "0.5";
+    body.style.pointerEvents = "none";
+}
+
+// Fonction image 2 de venus
+function appearimageVen() {
+    ven.style.display = "block";
+    btndis.style.display = "block";
+    body.style.opacity = "0.5";
+    body.style.pointerEvents = "none";
+}
+
+function closeJup() {
+    jup.style.display = "none";
+    mer.style.display = "none";
+    nep.style.display = "none";
+    sat.style.display = "none";
+    ven.style.display = "none";
+    btndis.style.display = "none";
+    body.style.opacity = "1";
+    body.style.pointerEvents = "auto";
+}
